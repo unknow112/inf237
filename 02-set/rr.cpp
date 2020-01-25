304cbad00887689195f3d9453731874baac1bdd0
Martin Tonhauzer <unknow112@gmail.com>
2020-01-25 16:16:06 +0100

    reversing-roads finally able to assign components

--- 02-set/rr.cpp
+++ 02-set/rr.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <unordered_set>
 #include <vector>
 #include <limits>
 
-const int UNDEFINED=-1;
-const int INFINITY= std::numeric_limits<int>::max();
+//const int UNDEFINED= std::numeric_limits<int>::max();
+//const int INFINITY= std::numeric_limits<int>::max();
+const int UNDEFINED = -1 ;
+
+//struct condensed_graph_t{
+//        condensed_graph_t(std::vector< edge_t> field) {
+//
+//
+//
+//
+//}
 
 struct edge_t
 {
@@ -11,55 +21,59 @@ struct edge_t
         //        neighbors_(n), in_(in), komp_(komp), esc_(esc)
         //        {}
         std::vector<int > neighbors_ = {};
+        std::vector<int > neighbors_rev_ = {};
         int index_ = UNDEFINED;
-        int in_ = UNDEFINED;
         int komp_ = UNDEFINED;
-        int esc_ = INFINITY; 
+        
 };
 
 
 class componentfinder 
 {
+public:
         componentfinder(std::vector<edge_t >& field):
                 field_(field){}
+                
 
         auto solve(){
-                for (auto & u: field_){
-                        if (u.in_ == UNDEFINED){
-                                tarjan(u);
-                        }
+                for (const auto& e: field_){
+                        dfs(e);
                 }
-                return field_;
+                int C_n = 0 ; 
+                for (auto index = S.rbegin() ; index != S.rend() ; index ++ ){
+                        if (field_[*index].komp_ == UNDEFINED){
+                                back_dfs(field_[*index], C_n);
+                                C_n ++ ;
+                        }  
+                } 
+
         }
-        
-        void tarjan(edge_t& v){
-                T++;
-                v.in_=T;
-                Z.push_back(v.index_);
-                v.esc_ = INFINITY;
-                for (auto&w: v.neighbors_){
-                        if (field_[w].in_ == UNDEFINED){
-                                v.esc_ = std::min(v.esc_, field_[w].in_);
-                        } else if (field_[w].komp_ == UNDEFINED) { 
-                                v.esc_ = std::min(v.esc_, field_[w].in_);
-                        }
-                }
-                if (v.esc_ >= v.in_){
-                        while (1){
-                                auto t = Z.back();
-                                Z.push_back(); 
-                                field_[t].komp_ = v.index_;
-                                if (t == v.index_){
-                                        break;
-                                }
-                        }
-                }
 
-                                
-        int T = 0 ;
-        std::vector<int> Z; 
+private: 
+        void back_dfs( edge_t& e, int komp_n){
+                if (e.komp_ != UNDEFINED){
+                        return;
+                }
+                e.komp_ = komp_n;
+                for (const auto next: e.neighbors_rev_){
+                        back_dfs(field_[next], komp_n);
+                }
+        }
+        void dfs(const edge_t& e){
+                if (visited_.find(e.index_) == visited_.end()){
+                        visited_.insert(e.index_);
+                } else {
+                        return; 
+                }
+                for (const auto& next: e.neighbors_){
+                        dfs(field_[next]);
+                }
+                S.push_back(e.index_);
+        }
+
+        std::unordered_set<int > visited_;        
+        std::vector<int> S; 
         std::vector<edge_t > field_; 
-
 };
 
 
@@ -81,8 +95,10 @@ int main()
                         int from , to ;
                         std::cin >> from >> to ;
                         field[from].neighbors_.push_back(to);
+                        field[to].neighbors_rev_.push_back(from);
                 }
-                field = componentfinder(field).solve();
+                //field = componentfinder(field).solve();
+                componentfinder(field).solve();
 
         }
 }
