9cd66d8be36e422c048cf41e5c5cf41f90f9240a
Martin Tonhauzer <xtonhauzer@stuba.sk>
2020-02-05 11:54:15 +0100

    ro possibly done, will compile

--- 04-dp/ro.cpp
+++ 04-dp/ro.cpp
@@ -1,3 +1,8 @@
+#include <cassert>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <map>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,29 +14,77 @@ struct item_t
         int price_=0;
 };
 
-std::unordered_map<int, std::pair<bool, std::vector<item_t > > > prices_combinations_ ; 
-
-solve(const std::vector<int> & costs, int price)
+struct solution_t
 {
-        auto c = std::lower_bount(
+	explicit solution_t(const std::multiset<int>& ans):
+		answer_(ans){}
+	
+	solution_t()
+	{}
+
+	bool is_unique_ = true;
+	std::multiset<int > answer_ = {}; 
+};
+
+std::unordered_map<int, solution_t > prices_combinations_ ; 
+
+
+
+const auto &solve(const std::vector<item_t> & costs, int price)
+{
+	// ak uz takuto vahu mam v sete, tak zbytocne hladat, pretoze uz som ju skusil vsetkymi sposobmi vytvorit
+	assert(price > 0);
+	auto solved = prices_combinations_.find(price);
+	if (solved != prices_combinations_.end()) { 
+		return (*solved).second;
+	}
+
+        auto item = std::lower_bound(
                 costs.begin(),
                 costs.end(),
                 price,
-                (const item_t& a, int P){ return a.price_ > P; }
+                [](const item_t& a, int P){ return a.price_ > P; }
         );
-        for (; c != costs.end() ; c++ ) { 
-                //1. c == price
-                        // tak to potom zapis do mapy a return
-                //2. c < price
-                        // tak to potom skus rekurzivne vyrobit mensojo z mensojo a ak podari:
-                                //  inheritni boolean ze viacero sposobov
-                                //  a ze pozri ci neni daco pred tym , ak je tak bool je true
-                // ak zistim ze aktualnu price viem dat viacero sposobmi, mozem prestat iterovat
-        }
-                                
-                
-        }
-                
+
+	prices_combinations_.insert({price,solution_t(std::multiset<int>())});
+	bool no_solution = true;
+
+        for (; item != costs.end() ; item++ ) { 
+		if ( (*item).price_ == price ) {
+			if (no_solution){
+				no_solution = false;
+				prices_combinations_.insert({price, solution_t({(*item).id_})});
+				continue;
+			} else {
+				prices_combinations_[price].is_unique_ = false;
+				return prices_combinations_[price];
+			}
+		}
+		if ((*item).price_ < price) { 
+			const auto &rest = solve(costs, price - (*item).price_);
+			if ( rest.answer_.empty() ) { 
+				continue;
+			} else { 
+				if (no_solution) {
+					no_solution = false ; 
+					auto solution = rest;
+					solution.answer_.insert((*item).id_);
+					prices_combinations_.insert({price, solution});
+					if (solution.is_unique_) {
+				       		continue;
+					} else { 
+						return prices_combinations_[price];
+					}								
+				} else { 
+					prices_combinations_[price].is_unique_ = false;
+					return prices_combinations_[price];
+				}
+			}
+		}	
+       		assert( (*item).price_ <= price );
+       	}
+        
+	return prices_combinations_[price];
 
 }
 
@@ -54,11 +107,12 @@ int main()
         std::sort(
                 costs.begin(), 
                 costs.end(), 
-                [](const item_t&a, const item_t& b){ return a.price_ > b.price_ } 
+                [](const item_t&a, const item_t& b){ return a.price_ > b.price_; } 
         );
  
 
         int m;
+	std::cin >> m ;
         for (int i = 0 ; i < m ; i ++ ) { 
                 int P;
                 std::cin >> P ;
