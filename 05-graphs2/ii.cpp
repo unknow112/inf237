36a7cbe1411079d5e1f8a0d9ed52c92141c38dcc
Martin Tonhauzer <xtonhauzer@stuba.sk>
2020-02-12 12:15:54 +0100

    [ii] untested prototype lol

---
+++ 05-graphs2/ii.cpp
@@ -0,0 +1,90 @@
+#include <cassert>
+#include <vector>
+#include <unordered_set>
+#include <iostream> 
+
+
+struct edge_t
+{
+        std::vector<int > neighbors_ = {};
+        std::vector<int > neighbors_rev_ = {};
+ 	int id_ = -1; 
+	bool is_baddie_ = false;       
+	
+};
+
+
+void rev_dfs(const std::vector<edge_t>&m, std::unordered_set<int >& singles, const edge_t& e )
+{
+	if ( singles.end() != singles.find(e.id_) ) {
+		return;
+	}
+
+	singles.insert(e.id_);
+	
+	for (auto r_neig : e.neighbors_rev_) { 
+		rev_dfs(m, singles, m[r_neig]);
+	}
+
+}	
+
+void dfs(const std::vector<edge_t>&m, std::unordered_set<int >& singles, const edge_t& e )
+{
+	if ( singles.end() != singles.find(e.id_) ) {
+		return;
+	}
+	singles.insert(e.id_);
+
+	for (auto neig : e.neighbors_) { 
+		dfs(m, singles, m[neig]);
+	}
+}
+
+
+int solve(const std::vector<edge_t> &m) 
+{
+	std::unordered_set<int > singles ; 
+
+	for(auto e = m.begin() ; e != m.end() ; e++ ) { 
+		if ((*e).is_baddie_) { 
+			rev_dfs(m,singles, *e);
+		}
+	}
+
+	int total_messages = singles.size();
+
+	for(auto e = m.begin() ; e != m.end() ; e++ ) { 
+		dfs(m, singles, *e); 
+		total_messages++; 
+	}
+	return total_messages;
+}
+
+
+int main () 
+{
+	int s , e , c;
+	std::cin >> s >> e >> c ; 
+
+	std::vector<edge_t> pavucinka{std::size_t(s)}; 
+	for(int i = 0 ; i < s ; i ++ ) { 
+		pavucinka[i].id_ = i ;
+	}
+
+	for (int i = 0 ; i < c ; i++) { 
+		int from, to ;
+		std::cin >> from >> to;
+		pavucinka[from].neighbors_.push_back(to);
+		pavucinka[to].neighbors_rev_.push_back(from);
+	}
+
+	for(int i = 0 ; i < e ; i++) { 
+		int rogue;
+		std::cin >> rogue;
+		pavucinka[rogue].is_baddie_ = true; 
+	}
+	
+	std::cout << solve(pavucinka);  	
+			
+	return 0 ;
+}
