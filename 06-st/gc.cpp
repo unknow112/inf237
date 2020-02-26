2b078949ea132c07acba08a1f67bc5ad7c68102e
Martin Tonhauzer <xtonhauzer@stuba.sk>
2020-02-26 02:47:21 +0100

    [gc] naive implementation accepted 7 of 22 cases

--- 06-st/gc.cpp
+++ 06-st/gc.cpp
@@ -6,11 +6,11 @@
 class perm_gen_t 
 {
 public:
-        explicit perm_gen_t(const std::vector<int>& inputs):
+        explicit perm_gen_t(std::vector<int>& inputs):
                 begin_(inputs.begin()), 
                 end_(inputs.end()) 
         {
-                assert(data_.size() > 1);
+                assert(inputs.size() > 1);
         }
         
         void first( std::vector<int>::iterator& curr_f , std::vector<int>::iterator& curr_s ) {
@@ -24,9 +24,9 @@ public:
                 }
 
                 curr_s++;
-                if (curr_s++ == end_){
+                if (curr_s == end_){
                         curr_f++;
-                        curr_s = (curr_s == end_) ? curr_s : curr_f +1;
+                        curr_s = (curr_f == end_) ? curr_f : curr_f +1;
                 }
         }
 
@@ -38,27 +38,40 @@ private:
 
 
 int main() { 
-        std::vector<int> a = {10,  7,  2,  4,  8,  6,  9,  3,  5,  1} ; 
-        std::vector<int> b = { 1, 10,  2,  9,  3,  5,  8,  7,  6,  4} ; 
-        std::vector<int> c = { 4,  8,  9, 10,  3,  1,  2,  6,  5,  7} ; 
+	int S ; 
+	std::cin >> S ; 
 
-        std::vector<int> b_pos{11};
-        std::vector<int> c_pos{11}; 
-        
-        for (int i = 0 ; i < 10 ; i ++ ) { 
+	auto a = std::vector<int> (S); 
+	for (int i = 0 ; i < S ; i++) { 
+		std::cin >> a[i]; 
+	}
+
+	auto b = std::vector<int> (S); 
+        auto b_pos = std::vector<int> (S +1);
+	for (int i = 0 ; i < S ; i++) { 
+		std::cin >> b[i]; 
                 b_pos[  b[i] ] = i;
+	}
+
+	auto c = std::vector<int> (S); 
+        auto c_pos = std::vector<int> (S +1); 
+	for (int i = 0 ; i < S ; i++) { 
+		std::cin >> c[i]; 
                 c_pos[  c[i] ] = i;
-        }
+	}
 
         auto perm = perm_gen_t(a) ; 
         decltype(a.begin()) x, y ; 
         perm.first(x,y);
         int count = 0 ; 
-        for ( ; x != a.end() || y != a.end() ; perm.next(x,y)) { 
-                cout += int( b_pos[*x] < b_pos[*z] && c_pos[*x] < c_pos[*y]       ); 
+        while (x != a.end() && y != a.end()) {  
+                if ( b_pos[*x] < b_pos[*y] && c_pos[*x] < c_pos[*y] ) { 
+			count++;
+		}
+ 		perm.next(x,y); 
         }
 
-        std::cout >> count >> std::endl;
+        std::cout << count << std::endl;
         return 0 ;
 }
              
