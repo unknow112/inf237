e2d798722baffa029ddc6cd833c28eb38e019a53
Martin Tonhauzer <xtonhauzer@stuba.sk>
2020-03-06 12:12:47 +0100

    [gc] normal solutino and addded many random inputs

--- 06-st/gc.cpp
+++ 06-st/gc.cpp
@@ -2,50 +2,101 @@
 #include <algorithm>
 #include <iostream>
 #include <vector> 
+#include <cstdint> 
+
+const int64_t undefined_c = -1 ; 
+const unsigned int node_border_m_ = 4 ; 
 
 
-class perm_gen_t 
+class counter_t
 {
 public:
-        explicit perm_gen_t(std::vector<int>& inputs):
-                begin_(inputs.begin()), 
-                end_(inputs.end()) 
-        {
-                assert(inputs.size() > 1);
-        }
-        
-        void first( std::vector<int>::iterator& curr_f , std::vector<int>::iterator& curr_s ) {
-                curr_f = begin_;
-                curr_s = begin_ + 1; 
-        }
+	explicit counter_t(std::size_t size):
+		mem_( size * node_border_m_, undefined_c ), 
+		size_(size)
+	{
+		init_(0, size_ -1,  0 );
+	}
 
-        void next( std::vector<int>::iterator& curr_f , std::vector<int>::iterator& curr_s ) {
-                if (curr_f == end_ && curr_s == end_){
-                        return; 
-                }
 
-                curr_s++;
-                if (curr_s == end_){
-                        curr_f++;
-                        curr_s = (curr_f == end_) ? curr_f : curr_f +1;
-                }
-        }
+	int64_t query(int ordn){
+		int64_t res = 0 ; 
+		if (ordn > 0) {
+			res = sum_(0, ordn -1, 0 , size_ -1, 0 ); 
+		}
+		count_(ordn);
+		return res;
+	}
 
-private: 
-        std::vector<int>::iterator begin_;
-        std::vector<int>::iterator end_;
+private:
+	void count_(const std::size_t pos){
 
+		std::size_t from = 0 ;
+		std::size_t to = size_ -1; 
+		std::vector<std::size_t> update_indexes ; 
+		std::size_t index = 0; 
+		while (! (from == to && to == pos)) {
+			update_indexes.push_back(index);
+			int half = int((to - from) / 2 ) ;
+		  	if ( from + half < pos ) {
+			        index = 2*index + 2; 
+				from = from + half +1 ;
+			} else { 
+				index = 2*index + 1;
+				to = from + half;
+			}	
+		}
+		mem_.at(index) ++ ;
+		for (auto s_index = update_indexes.rbegin(); s_index != update_indexes.rend() ; s_index++ ) { 
+			mem_.at(*s_index) ++;
+		}
+	}
+
+	int64_t sum_(std::size_t from, std::size_t to, std::size_t bfrom, std::size_t bto, std::size_t store_index){
+		// 1. exact range
+		// 2. prienik
+		// 3. ziadny prienik
+		assert(from <= to); 
+		if (from <= bfrom && to >= bto) {
+			return mem_.at(store_index); 
+		} 
+		if ( from > bto || to < bfrom ) {
+			return 0;
+		}
+		if (from <= bto || to >= bfrom ) {
+			int half = int((bto - bfrom) / 2 ) ;
+			return   sum_(from, to, bfrom, bfrom + half,  2*store_index +1) 
+			       + sum_(from, to, bfrom + half +1, bto, 2*store_index +2);	
+		}
+		assert(!"Whoa Buddy"); 
+		return -1;	
+	}
+
+	void init_(std::size_t from, std::size_t to, std::size_t index){
+		assert(from <= to);
+		mem_.at(index) = 0 ; 
+		if (from == to) {
+			return ;
+		}
+			
+		int half = int((to - from) / 2 ) ;
+		init_(from,  from + half, 2*index +1 ) ;
+		init_(from + half +1, to, 2*index +2 ) ; 
+
+	}
+	std::vector<int64_t> mem_ ; 
+	std::size_t size_ ; 
 };
 
-struct num_meta_t
-{
-	int most_left_pos_ ;
-	std::vector<int>::iterator l_iter_;
 
-	int most_right_pos_ ;
-	std::vector<int>::iterator r_iter_;
 
-}; 
+
+
+
+
+
+
+
 using intarr_t = std::vector<int> ;
 
 struct foo_t 
@@ -59,6 +110,28 @@ struct foo_t
 	intarr_t ord_;	
 	intarr_t pos_;
 };
+
+int64_t get_result(int64_t AB, int64_t AC, int64_t BC, int S)
+{
+	const int64_t T = (S*(S-1))/2;
+	return (AB+AC+BC-T)/2;
+}
+
+
+int64_t pairs(const foo_t& main, const foo_t& sec, int S)
+{
+	int64_t res = 0 ; 
+
+	auto ord = counter_t(S);
+
+	for (auto num : sec.ord_) {
+	
+		res += ord.query(main.pos_[num]);
+	
+	}
+
+	return res;
+}
 
 int main() { 
 	int S ; 
@@ -83,29 +156,12 @@ int main() {
                 c.pos_[  c.ord_[i] ] = i;
 	}
 
-	int count = 0 ;
 
-	for (int cyslo = 1 ; cyslo  <= S ; cyslo ++ ) { 
-		auto& leftmost_vec = *std::max(
-			{ &a, &b, &c } , 
-			[cyslo](const foo_t* x, const foo_t *y ){
-				return (*x).pos_[cyslo] < (*y).pos_[cyslo];
-		       	} 
-		);
-		for (   auto druhe = leftmost_vec.ord_.begin() + leftmost_vec.pos_[cyslo] + 1;
-		  	druhe != leftmost_vec.ord_.end() ;
-			druhe ++   	  
-		) {
-			if (	a.pos_[cyslo] < a.pos_[*druhe]
-			     && b.pos_[cyslo] < b.pos_[*druhe] 
-			     && c.pos_[cyslo] < c.pos_[*druhe] ) { 
-				count++;
-			}
-		}	
-		
-	}
-        std::cout << count << std::endl;
-        return 0 ;
+	int64_t ab = pairs(a,b,S);
+	int64_t ac = pairs(a,c,S);
+	int64_t bc = pairs(b,c,S);
+	std::cout << get_result(ab,ac,bc,S) ;
+	return 0 ;
 	
 	
 }
