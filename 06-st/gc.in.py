46b961ea51b845d99624f0ed42d93b3a77962784
Martin Tonhauzer <xtonhauzer@stuba.sk>
2020-03-06 21:40:07 +0100

    [gc] made generator more script friendly

--- 06-st/gc.in.py
+++ 06-st/gc.in.py
@@ -4,16 +4,14 @@ from os import scandir
 
 
 
-try: 
-    S = int(argv[1])
-except (ValueError, IndexError):
-    print("supply valid  2<= int <= 200000 as first arg!", file=stderr)
-    exit(1)
 
-print(S)
-print(' '.join(map(str,np.random.permutation(S) + 1 )))
-print(' '.join(map(str,np.random.permutation(S) + 1 )))
-print(' '.join(map(str,np.random.permutation(S) + 1 )))
+def generate_input(S): 
+    return '\n'.join([
+        str(S),
+        ' '.join(map(str,np.random.permutation(S) + 1 )),
+        ' '.join(map(str,np.random.permutation(S) + 1 )),
+        ' '.join(map(str,np.random.permutation(S) + 1 ))
+    ])
 
 
 
@@ -22,4 +20,10 @@ print(' '.join(map(str,np.random.permutation(S) + 1 )))
 
 
 
-
+if __name__ == "__main__": 
+    try: 
+        S = int(argv[1])
+        print(generate_input(S))
+    except (ValueError, IndexError):
+        print("supply valid  2<= int <= 200000 as first arg!", file=stderr)
+        exit(1)
