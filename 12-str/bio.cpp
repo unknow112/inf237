36ebda02285514596ec3576d17d5b3f72fd55489
Martin Tonhauzer <xtonhauzer@stuba.sk>
2020-04-21 13:05:35 +0200

    [bio] wrong answer they say pfft

--- 12-str/bio.cpp
+++ 12-str/bio.cpp
@@ -1,15 +1,41 @@
+#include <cassert>
 #include <iostream>
-#include <unordered_set>
 #include <string>
+#include <vector>
+#include <array>
 
-std::unordered_multiset<std::string> INPUT;
+struct node_t
+{
+	node_t(std::size_t id, char lett):
+		id(id), lett(lett)
+	{}
+	std::size_t id;
+	int count = 0;
+	char lett;
+	std::array<int, 26> next = {-1, -1, -1, -1, 
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, 
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, 
+		-1, -1, -1, -1 };
+};
 
-auto find_and_insert(std::string &s){
-	auto result = INPUT.count(s);
-	for (int i = 1;  i <= int(s.size()) ; i ++ ){
-		INPUT.insert(s.substr(0,i));
+auto INPUT = std::vector<node_t>();
+
+auto find_and_insert(const char *s, node_t &pos )
+{
+	assert(pos.id == 0 || *s == pos.lett);
+	int res = pos.count;
+	pos.count++; 
+	if ('\0' == s[1]) {
+		return res;
+	} else {
+		char index = s[1] - 'a';
+       		if (pos.next[index] == -1){
+			auto newid = INPUT.size();
+			INPUT.push_back({newid, s[1]});
+			pos.next[index] = newid;
+		}
+		return find_and_insert(s+1, INPUT[pos.next[index]]);
 	}
-	return result;
 }	
 
 
@@ -18,10 +44,11 @@ int main()
 	int n ; 
 	std::cin >> n;
 	INPUT.reserve(n*32);
+	INPUT.push_back({0,'\0'});
 	for (int i = 0 ; i < n ; i++ ) { 
 		std::string s;
 		std::cin >> s;
-	        std::cout << find_and_insert(s) << '\n';	
+	        std::cout << find_and_insert(s.c_str(), INPUT.front()) << '\n';	
 	}
 
 	return 0;
