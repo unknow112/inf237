576d54c3807d98537b44b550663697d8d49557e5
Martin Tonhauzer <xtonhauzer@stuba.sk>
2020-05-11 11:56:55 +0200

    bing it on fokume done

--- 12-str/bio.cpp
+++ 12-str/bio.cpp
@@ -12,30 +12,29 @@ struct node_t
 	std::size_t id;
 	int count = 0;
 	char lett;
-	std::array<int, 26> next = {-1, -1, -1, -1, 
+	std::array<int, 27> next = {-1, -1, -1, -1, 
 		-1, -1, -1, -1, -1, -1, -1, -1, -1, 
 		-1, -1, -1, -1, -1, -1, -1, -1, -1, 
-		-1, -1, -1, -1 };
+		-1, -1, -1, -1, -1 };
 };
 
 auto INPUT = std::vector<node_t>();
 
-auto find_and_insert(const char *s, node_t &pos )
+auto find_and_insert(const char *s, int parent)
 {
-	assert(pos.id == 0 || *s == pos.lett);
-	int res = pos.count;
-	pos.count++; 
-	if ('\0' == s[1]) {
-		return res;
-	} else {
-		char index = s[1] - 'a';
-       		if (pos.next[index] == -1){
-			auto newid = INPUT.size();
-			INPUT.push_back({newid, s[1]});
-			pos.next[index] = newid;
-		}
-		return find_and_insert(s+1, INPUT[pos.next[index]]);
+	auto res = INPUT[parent].count; 
+	INPUT[parent].count++;
+	if ( '\0' == *s) {
+		return res; 
 	}
+	char my_index = *s - 'a';
+	if (INPUT[parent].next[my_index] == -1){
+		auto newid = INPUT.size();
+		INPUT.push_back({newid, *s});
+		INPUT[parent].next[my_index] = newid;
+	}
+	return find_and_insert(s+1, INPUT[parent].next[my_index]);
+	
 }	
 
 
@@ -48,7 +47,7 @@ int main()
 	for (int i = 0 ; i < n ; i++ ) { 
 		std::string s;
 		std::cin >> s;
-	        std::cout << find_and_insert(s.c_str(), INPUT.front()) << '\n';	
+	        std::cout << find_and_insert(s.c_str(), 0 ) << '\n';	
 	}
 
 	return 0;
